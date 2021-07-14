854c9eda98591ddc3aac493c181415109aeaef95
Dmitri <dmitri.yatchenko@gmail.com>
2021-07-14 21:33:37 +0300

    done sout for main class

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -7,12 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello world");
         System.out.println("Введите строку для анализа повторяющихся триад");
         Scanner scanner = new Scanner(new BufferedInputStream(System.in));
 
         String line =  scanner.nextLine();
 
-        NumberTriplets numberTriplets = new NumberTriplets();
+        NumberTriplets numberTriplets = new NumberTriplets();//
         System.out.println("Заданная сторока: " + line);
         System.out.println("Количество \"троек\" в заданной строке: " + numberTriplets.numberOfTriplets(line));
 
